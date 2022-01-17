5c814fbbe21063a8b08625f618d1694349604f47
AlexBCSouza <alex.batista.91@gmail.com>
2022-01-17 17:01:30 -0300

    API JavaScript

--- restApi/src/app.js
+++ restApi/src/app.js
@@ -2,29 +2,30 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const req = require('express/lib/request');
-const res = require('express/lib/response');
 
 class App {
-    constructor(){
+    constructor() {
         this.app = express();
-        this.middlewares();
+        this.middleware();
         this.routes();
+
     }
-    middlewares() {
+    middleware() {
         this.app.use(express.json());
 
-
-        this.app.use((req, res, next) => {
-            res.header("access-Controll-Allow-Origin", "*");
-            res.header("access-Controll-Allow-Methods", "GET, POST, PUT, DELETE");
-            res.header("access-Controll-Allow-headers", "access, contenty-type, Authorization, Acept, Oringin, X-Requested-With");
-
+        this.app.use((req, res, next) =>{
+            res.header("Access-Controll-Allow-Origin", "*");
+            res.header("Access-Controll-Allow-Methods", "Get, POST, PUT, DELETE");
+            res.header("Access-Controll-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-With");
             this.app.use(cors());
             next();
         })
+
     }
+
     routes() {
         this.app.use(routes);
+
     }
 }
-module.exports = new App().app;
+module.express = new App().app;
--- restApi/src/routes.js
+++ restApi/src/routes.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
 
+const UserController = require('./app/Controllers/UserController');
+
 const routes = new Router();
 
-routes.post("/", );
+routes.post("/", UserController.index );
+
+module.exports = routes;
